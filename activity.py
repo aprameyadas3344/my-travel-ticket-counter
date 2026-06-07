52a6b64dca4f08503aafad8ad220623bbddc1757
aprameyadas3344 <gojosatoru.aprameyadas@gmail.com>
2026-06-07 10:36:06 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,18 @@
+# my travel ticket counter
+passenger_name = "aprameya das"
+destination = "goa"
+ticket_price = float(850.50)
+number_of_tickets = 3
+is_available = bool(True)
+morning_ticket = 700
+evening_ticket = 900
+temp = evening_ticket
+morning_ticket = temp
+evening_ticket = morning_ticket
+ 
+print("passenger name is", passenger_name)
+print("destination is", destination)
+print("ticket price is", ticket_price)
+print("available tickets", is_available)
+print("after swaping prices morning tickets cost", morning_ticket)
+print("after swaping prices evening tickets cost", evening_ticket)
